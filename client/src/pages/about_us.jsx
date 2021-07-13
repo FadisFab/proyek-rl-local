6681cb83146344be5403c9f685b1460945ddc611
FadisFab <mochamad.fadillah.tif419@polban.ac.id>
2021-07-13 21:09:54 +0700

    front-end(Proyek-2): Adding Pages [Landing Page, Dashboard, Explaination]

---
+++ client/src/pages/about_us.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Nav from './Nav';
+import "../css/about-us.css";
+
+function about_us() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>This is About Us Page</h1>
+    </div>
+  );
+}
+
+export default about_us;
